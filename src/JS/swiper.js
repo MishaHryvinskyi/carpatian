429a2b2a8313032b957b3f481523730cddd93a76
Misha <hryvinskyim@gmail.com>
2025-03-03 21:45:31 +0200

    gallery

---
+++ src/JS/swiper.js
@@ -0,0 +1,19 @@
+import Swiper from 'swiper';
+
+export const gallerySwiper = new Swiper(".mySwiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".gallery__button-next",
+      prevEl: ".gallery__button-prev",
+    },
+    spaceBetween: 20,
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      375: { slidesPerView: 1.1 },
+      768: { slidesPerView: 2 },
+      1440: { slidesPerView: 2 },
+    },
+    keyboard: { enabled: true },
+    autoplay: { delay: 5000 },
+    speed: 1000,
+  });
