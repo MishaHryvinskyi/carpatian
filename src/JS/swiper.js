3346bdedf3653ea09b5c9e9d9e0f882e811b2919
Misha <hryvinskyim@gmail.com>
2025-03-04 19:25:02 +0200

    markup and styled upcoming section for mobile

--- src/JS/swiper.js
+++ src/JS/swiper.js
@@ -15,3 +15,22 @@ export const gallerySwiper = new Swiper(".mySwiper", {
     autoplay: { delay: 5000 },
     speed: 1000,
   });
+
+
+  export const upcomingSwiper = new Swiper(".upcomingSwiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".gallery__button-next",
+      prevEl: ".gallery__button-prev",
+    },
+    spaceBetween: 20,
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      375: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1440: { slidesPerView: 3 },
+    },
+    keyboard: { enabled: true },
+    autoplay: { delay: 5000 },
+    speed: 1000,
+  });
