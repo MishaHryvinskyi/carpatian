94c0d3cdd5c31c81972759fbc91001b9b151aa5e
MishaHryvinskyi <116170668+MishaHryvinskyi@users.noreply.github.com>
2025-03-04 14:02:43 +0000

    Deploying to gh-pages from @ MishaHryvinskyi/yoga@827cd4abbb40ec24bfa0eeb6416fdb824e878bc7 🚀

--- index.4a6e8c19.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{loop:!0,navigation:{nextEl:".gallery__button-next",prevEl:".gallery__button-prev"},spaceBetween:20,breakpoints:{320:{slidesPerView:1},375:{slidesPerView:1.1},768:{slidesPerView:2},1440:{slidesPerView:2}},keyboard:{enabled:!0},autoplay:{delay:5e3},speed:1e3});
-//# sourceMappingURL=index.4a6e8c19.js.map
